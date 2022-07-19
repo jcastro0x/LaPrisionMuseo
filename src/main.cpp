256fe24f851c199bb9306934d94557675622446c
jcastro0x <jcastro0x@gmail.com>
2022-07-19 07:43:12 +0200

    working on QuadAspectRatio

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,12 @@
 
 #include <widgets/QuadAspectRatio.h>
 
-float left;
-float right;
-float top;
-float bottom;
-/**
- [0]----[1]
-  .      .
-  .      .
- [3]----[2]
- */
-void resize(sf::VertexArray& va, sf::Vector2u winSize, float aspectRatio)
-{
-    left   = 0.f;
-    right  = static_cast<float>(winSize.y) * aspectRatio;
-    top    = 0.f;
-    bottom = static_cast<float>(winSize.y);
 
 
-
-    //va[0].position = sf::Vector2f(left, top);
-    //va[1].position = sf::Vector2f(right, top);
-    //va[2].position = sf::Vector2f(right, bottom);
-    //va[3].position = sf::Vector2f(left, bottom);
-
-    va[0].position = sf::Vector2f(0,   0);
-    va[1].position = sf::Vector2f(100, 0);
-    va[2].position = sf::Vector2f(100, bottom);
-    va[3].position = sf::Vector2f(0,   bottom);
-}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "La Prision - Museo");
+    sf::RenderWindow window(sf::VideoMode(640, 580), "La Prision - Museo");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
@@ -49,24 +22,18 @@ int main()
 
     QuadAspectRatio quadAspectRatio;
 
-    sio::client client;
-
-    client.set_open_listener([](){
-        std::cout << "Connected\n";
-    });
-    client.set_close_listener([](sio::client::close_reason const& reason){
-        std::cout << "Disconnected\n";
-    });
-    client.set_fail_listener([](){
-        std::cout << "Fail Connected\n";
-    });
-    client.connect("wss://testserv.prisonserver.net:5000");
-
-    /*
-     Hi, if you want to use HTTPS you need to make sure that you built the TLS version of the
-     socket.io C++ library. To do so you need to link against the OpenSSL libraries and pass the
-     SIO_TLS preprocessor definition when compiling.
-     */
+//    sio::client client;
+//
+//    client.set_open_listener([](){
+//        std::cout << "Connected\n";
+//    });
+//    client.set_close_listener([](sio::client::close_reason const& reason){
+//        std::cout << "Disconnected\n";
+//    });
+//    client.set_fail_listener([](){
+//        std::cout << "Fail Connected\n";
+//    });
+//    client.connect("wss://testserv.prisonserver.net:5000");
 
     sf::Event event {};
     while (window.isOpen())
@@ -84,15 +51,16 @@ int main()
 
                 case sf::Event::Resized:
 
-                    sf::FloatRect visibleArea(
+                    event.size.width = std::max(640u, event.size.width);
+                    event.size.height = std::max(480u, event.size.height);
+                    window.setSize({event.size.width, event.size.height});
+
+                    window.setView(sf::View({
                         0.f,
                         0.f,
                         static_cast<float>(event.size.width),
                         static_cast<float>(event.size.height)
-                    );
-                    window.setView(sf::View(visibleArea));
-
-                    //resize(background, sf::Vector2u(event.size.width, event.size.height), textureAspectRationWidth);
+                    }));
                     break;
             }
 
@@ -102,14 +70,7 @@ int main()
 
         //ImGui::ShowDemoWindow();
 
-        ImGui::Begin("Redraw");
-        ImGui::LabelText("Left",    "Left: %f",   left);
-        ImGui::LabelText("Right",   "Right: %f",  right);
-        ImGui::LabelText("Top",     "Top: %f",    top);
-        ImGui::LabelText("Bottom",  "Bottom: %f", bottom);
-        ImGui::Spacing();
-        ImGui::LabelText("Window", "x: %d y: %d", window.getSize().x, window.getSize().y);
-        ImGui::End();
+
 
         window.clear();
         window.draw(quadAspectRatio);
--- src/widgets/QuadAspectRatio.cpp
+++ src/widgets/QuadAspectRatio.cpp
@@ -22,6 +22,8 @@
 #include "QuadAspectRatio.h"
 #include <SFML/Graphics/RenderTarget.hpp>
 
+#include <imgui.h>
+
 /**
  [0]----[1]
   .      .
@@ -32,13 +34,15 @@
 QuadAspectRatio::QuadAspectRatio()
 : vertices_(sf::Quads, 4)
 {
-    texture_.loadFromFile("AL_Almacen1.jpg");
+
+    //texture_.loadFromFile("AL_Almacen1.jpg");
+    texture_.loadFromFile("checker640x480.png");
     texture_.setSmooth(false);
     texture_.setSrgb(false);
     texture_.setRepeated(false);
 
-    aspectRatio_ = static_cast<float>(texture_.getSize().x)
-                 / static_cast<float>(texture_.getSize().y);
+    aspectRatio_ = static_cast<float>(texture_.getSize().y)
+                 / static_cast<float>(texture_.getSize().x);
 
     const sf::Vector2f texSizeF = {static_cast<float>(texture_.getSize().x), static_cast<float>(texture_.getSize().y)};
 
@@ -46,28 +50,44 @@ QuadAspectRatio::QuadAspectRatio()
     vertices_[1].texCoords = { texSizeF.x, 0.f};
     vertices_[2].texCoords = { texSizeF.x, texSizeF.y};
     vertices_[3].texCoords = { 0.f,        texSizeF.y};
-
-//    vertices_[0].color = sf::Color::Red;
-//    vertices_[1].color = sf::Color::Green;
-//    vertices_[2].color = sf::Color::Blue;
-//    vertices_[3].color = sf::Color::Yellow;
-
-    vertices_[0].position = sf::Vector2f(0,   0);
-    vertices_[1].position = sf::Vector2f(100, 0);
-    vertices_[2].position = sf::Vector2f(100, 100);
-    vertices_[3].position = sf::Vector2f(0,   100);
 }
 
+#define VtxToStr(V) V.position.x, V.position.y
 void QuadAspectRatio::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     states.texture = &texture_;
 
-    const sf::Vector2f tarSizeF = {static_cast<float>(target.getSize().x), static_cast<float>(target.getSize().y)};
 
-    vertices_[0].position = sf::Vector2f(0,   0);
-    vertices_[1].position = sf::Vector2f(640, 0);
-    vertices_[2].position = sf::Vector2f(640, 480);
-    vertices_[3].position = sf::Vector2f(0,   480);
+    const sf::Vector2f tarSizeF   = {static_cast<float>(target.getSize().x), static_cast<float>(target.getSize().y)};
+
+    const auto targetAspectRatio = tarSizeF.y / tarSizeF.x;
+    const auto deltaAspectRatio  = .0f + (targetAspectRatio - aspectRatio_);
+
+    auto heightDiff = static_cast<float>(target.getSize().y - texture_.getSize().y);
+    auto widthDiff  = static_cast<float>(target.getSize().x - texture_.getSize().x);
+
+    vertices_[0].position = sf::Vector2f(0,             heightDiff / 2 - widthDiff / 2);
+    vertices_[1].position = sf::Vector2f(tarSizeF.x,    heightDiff / 2 - widthDiff / 2);
+    vertices_[2].position = sf::Vector2f(tarSizeF.x,    480 + heightDiff / 2 + widthDiff / 2);
+    vertices_[3].position = sf::Vector2f(0,             480 + heightDiff / 2 + widthDiff / 2);
+
+
+
+    ImGui::Begin("Redraw");
+    ImGui::LabelText("Window", "x: %d y: %d", target.getSize().x, target.getSize().y);
+    ImGui::LabelText("AR", "%.4f", deltaAspectRatio);
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::LabelText("0", "(x: %.2f y: %.2f)", VtxToStr(vertices_[0]));
+    ImGui::LabelText("1", "(x: %.2f y: %.2f)", VtxToStr(vertices_[1]));
+    ImGui::LabelText("2", "(x: %.2f y: %.2f)", VtxToStr(vertices_[2]));
+    ImGui::LabelText("3", "(x: %.2f y: %.2f)", VtxToStr(vertices_[3]));
+    ImGui::Spacing();
+    ImGui::Spacing();
+    ImGui::LabelText("TextSiz", "(x: %.2f y: %.2f)", vertices_[1].position.x - vertices_[0].position.x, vertices_[3].position.y - vertices_[0].position.y);
+    ImGui::LabelText("TextAR", "%.4f", (vertices_[1].position.x - vertices_[0].position.x) / (vertices_[3].position.y - vertices_[0].position.y));
+
+    ImGui::End();
 
     target.draw(vertices_, states);
 }
