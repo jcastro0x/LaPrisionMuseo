164ef167ab37681e0b23d6e4c4c546e71fb83476
jcastro0x <jcastro0x@gmail.com>
2022-08-03 03:20:07 +0200

    Program.cpp improved & fixed
    - Removed old code wrapped by #if 0
    - Fixed #WIN32 handle_signal

--- src/Program.cpp
+++ src/Program.cpp
@@ -279,7 +279,7 @@ int main(const int, const char**)
     signal(SIGABRT,  &handle_signals);
 
     #ifdef _WIN32
-    signal(SIGBREAK, &handle_aborts);
+    signal(SIGBREAK, &handle_signals);
     #endif
 
 
@@ -287,91 +287,4 @@ int main(const int, const char**)
     engine.run();
 
     return EXIT_SUCCESS;
-
-    #if 0
-    sf::RenderWindow window(sf::VideoMode(640, 580), "La Prision - Museo");
-    window.setFramerateLimit(60);
-    
-    ImGui::SFML::Init(window);
-
-    sf::Clock deltaClock;
-
-    QuadAspectRatio quadAspectRatio("AL_Almacen1.jpg", QuadAspectRatio::EAspectRatioRule::FitToParent);
-
-    sf::Texture maskTex;
-    maskTex.loadFromFile("AL_Almacen1_Mask.png");
-    maskTex.setRepeated(false);
-    maskTex.setSrgb(false);
-    maskTex.setSmooth(false);
-    auto maskImg = maskTex.copyToImage();
-
-    sf::Texture maskTexDbg;
-    maskTexDbg.create(64, 64);
-    sf::Sprite maskSprDbg(maskTexDbg);
-
-
-
-
-    sf::Event event {};
-    while (window.isOpen())
-    {
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(window, event);
-
-            switch (event.type) {
-                default:break;
-
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-
-                case sf::Event::Resized:
-
-                    event.size.width = std::max(640u, event.size.width);
-                    event.size.height = std::max(480u, event.size.height);
-                    window.setSize({event.size.width, event.size.height});
-
-                    window.setView(sf::View({
-                        0.f,
-                        0.f,
-                        static_cast<float>(event.size.width),
-                        static_cast<float>(event.size.height)
-                    }));
-                    break;
-            }
-
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        //ImGui::ShowDemoWindow();
-
-        auto coord = quadAspectRatio.transformPointToTextureCoords(window, sf::Mouse::getPosition(window));
-        sf::Color c = sf::Color::Transparent;
-        if(coord)
-        {
-            c = maskImg.getPixel(coord->x, coord->y);
-        }
-
-
-        ImGui::Begin("Mask color");
-        ImGui::ImageButton(maskSprDbg, 0, c);
-        ImGui::LabelText("Mask" ,  "x: %d y: %d", maskImg.getSize().x, maskImg.getSize().y);
-        if(coord)
-        {
-            ImGui::LabelText("Coord" , "x: %d y: %d", coord->x, coord->y);
-        }
-        ImGui::End();
-
-
-        window.clear();
-        window.draw(quadAspectRatio);
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
-    return EXIT_SUCCESS;
-    #endif
 }
